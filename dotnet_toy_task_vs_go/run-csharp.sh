42b2e7bb6fea6797cb10c78f233762645e4d73a7
kjpgit <kjpgit@users.noreply.github.com>
2019-01-30 10:28:34 -0600

    add run name

--- dotnet_toy_task_vs_go/run-csharp.sh
+++ dotnet_toy_task_vs_go/run-csharp.sh
@@ -2,7 +2,13 @@
 set -e
 cd `dirname $0`
 
+# Slight regression for 2.2
+#export COMPlus_TieredCompilation=1
+
 dotnet build -c Release 
+
+# Optional run name
+RUN_NAME=$1
 
 # Docs on CLR env variables:
 # https://github.com/dotnet/coreclr/blob/master/Documentation/project-docs/clr-configuration-knobs.md
@@ -11,6 +17,6 @@ for nr_threads in 1 2 4; do
     echo "Running, nr_threads=${nr_threads}"
     export COMPlus_ThreadPool_ForceMaxWorkerThreads=${nr_threads}
     export COMPlus_ThreadPool_ForceMinWorkerThreads=${nr_threads}
-    output_file=results/csharp_1m_${nr_threads}.txt
+    output_file=results/csharp_1m${RUN_NAME}_${nr_threads}.txt
     /usr/bin/time -v dotnet ./bin/Release/netcoreapp2.2/project.dll 1000000 1.0 60000000 &> $output_file
 done
